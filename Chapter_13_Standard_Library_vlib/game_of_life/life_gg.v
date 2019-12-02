73209ee8249ea346e0ce2786376ce0ee3d61b0d2
Ivo Balbaert <ivo.balbaert@telenet.be>
2019-12-02 11:37:31 +0100

    added new examples 0.1.23

--- Chapter_13_Standard_Library_vlib/game_of_life/life_gg.v
+++ Chapter_13_Standard_Library_vlib/game_of_life/life_gg.v
@@ -29,12 +29,12 @@ const (
 	graphics = new_graphics()
 )
 
-[live]
+// [live]
 fn print_automaton(a &automaton.Automaton){
 	gg.clear(gx.White)
 	square_size := 18
-	for y := 1; y<a.field.maxy; y++ {
-		for x := 1; x<a.field.maxx; x++ {
+	for y := 1; y < a.field.maxy; y++ {
+		for x := 1; x < a.field.maxx; x++ {
 			cell := a.field.get(x,y)
 			if cell == 1 {
 				graphics.draw_rect( square_size*x, square_size*y, square_size, square_size, filled_color )
@@ -48,11 +48,9 @@ fn main() {
 	for {
 		if graphics.window.should_close() { graphics.window.destroy() break }
 		gg.post_empty_event() // needed so the animation does not stop
-		///////////////////////////////////////////////
 		a.update()
 		print_automaton(a)
-		
 		graphics.render()
-		time.sleep_ms(1) // TODO: remove this when live reload depence on the time module is fixed
+		time.sleep_ms(1) // TODO: remove this when live reload dependence on the time module is fixed
 	}
 }
